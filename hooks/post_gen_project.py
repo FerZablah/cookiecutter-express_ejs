690fd137aa87cdd0959c59409749365fe8ebebc6
Fernando Zablah <zablah.developer@gmail.com>
2020-08-09 23:54:21 -0500

    Updated hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,4 @@
-bashCommand = "npm i && npm run dev"
+bashCommand = "npm i && npm run dev && open "localhost:4000"
 import subprocess
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
